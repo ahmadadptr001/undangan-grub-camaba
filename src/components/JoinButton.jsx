e3b1deaba1d753ee47641748e6f7ca8e9a37e26d
ahmadadptr001 <ahmadadptr@gmail.com>
2025-07-03 21:41:57 +0700

    perbaiki responsive ui

--- src/components/JoinButton.jsx
+++ src/components/JoinButton.jsx
@@ -7,9 +7,9 @@ const JoinButton = () => {
                 href="https://chat.whatsapp.com/Eog0QR1I8Jy41Pru8WHCJA?mode=r_t"
                 target="_blank"
                 rel="noopener noreferrer"
-                className="text-center justify-center flex-wrap flex items-center gap-1 bg-red-700 hover:bg-red-800 text-white font-bold px-6 py-3 rounded-full shadow-lg transition duration-300 tracking-wider"
+                className="text-xs sm:text-base text-center justify-center  flex items-center gap-1 bg-red-700 hover:bg-red-800 text-white font-bold px-6 py-3 rounded-full shadow-lg transition duration-300 tracking-wider"
             >
-                MASUK KE FAKULTAS TEKNIK <FaWhatsapp className=" rounded-full w-7 h-7"/>
+                MASUK KE FAKULTAS TEKNIK
             </a>
             <p className="mt-4 text-xs text-zinc-500 italic">Grup ini hanya untuk yang bermental baja.</p>
         </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
     return (
         <>
             <div className="fixed w-full h-full -z-1 inset-0">
-                    <Lightning hue={331} xOffset={0} speed={1} intensity={1.9} size={2} />
+                    <Lightning hue={331} xOffset={0} speed={1} intensity={1.6} size={2} />
             </div>
             <Layout />
         </>
