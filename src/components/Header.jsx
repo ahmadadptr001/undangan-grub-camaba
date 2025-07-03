74707ed13df8f79c16ec84dea40339e0457df4ee
ahmadadptr001 <ahmadadptr@gmail.com>
2025-07-03 21:51:40 +0700

    memperbaiki responsive pada image header dan title

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,9 +6,9 @@ const Header = () => {
             <img
                 src={ripImage}
                 alt="gambar rip"
-                className="mx-auto w-40 h-40 mb-4 rounded-full border-4 border-red-600 shadow-red-700 shadow-lg"
+                className="mx-auto sm:w-40 sm:h-40 w-30 h-30 mb-4 rounded-full border-4 border-red-600 shadow-red-700 shadow-lg"
             />
-            <h1 className="text-3xl font-extrabold text-red-600 drop-shadow-md">UNDANGAN <br /> FAKULTAS TEKNIK</h1>
+            <h1 className="text-2xl sm:text-3xl font-extrabold text-red-600 drop-shadow-md">UNDANGAN <br /> FAKULTAS TEKNIK</h1>
             <h2 className="text-xl font-semibold text-zinc-300 mt-3">Camaba FT-UNUSRA 2025</h2>
         </div>
     );
