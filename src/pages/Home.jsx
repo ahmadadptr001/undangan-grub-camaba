edf5695043cf1bda0e00152962e74fc06e317733
ahmadadptr001 <ahmadadptr@gmail.com>
2025-07-03 21:35:17 +0700

    second commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,19 +2,16 @@ import Header from "../components/Header";
 import Description from "../components/Description";
 import JoinButton from "../components/JoinButton";
 import Lightning from "../ui/Lightning/Lightning";
+import Layout from "../layout/Layout";
 
 const Home = () => {
     return (
-        <div className="min-h-screen  flex items-center justify-center p-4">
-            <div className="fixed w-full h-full -z-1">
-                <Lightning hue={331} xOffset={0} speed={1} intensity={1.9} size={2} />
+        <>
+            <div className="fixed w-full h-full -z-1 inset-0">
+                    <Lightning hue={331} xOffset={0} speed={1} intensity={1.9} size={2} />
             </div>
-            <div className="bg-zinc-900/95 rounded-3xl border border-red-800 shadow-lg/60 shadow-red-500 max-w-md w-full p-6">
-                <Header />
-                <Description />
-                <JoinButton />
-            </div>
-        </div>
+            <Layout />
+        </>
     );
 };
 
